2fbe95901b7210463f61a894395a0c61548e4e02
LiamOJ <48185797+LiamOJ@users.noreply.github.com>
2021-02-25 08:20:22 +0000

    Add files via upload

---
+++ Sysmon_Analyser.ps1
@@ -0,0 +1,32 @@
+﻿$time_period = 24
+$id_hash_table_for_counting = @{}
+
+if ( -not ($all_events.length -gt 0) ) {
+    $all_events = Get-WinEvent -FilterHashtable @{
+    logname="microsoft-windows-sysmon/operational";
+    starttime=(Get-date).addhours(-$time_period);
+    };
+}
+
+# Total logs
+Write-Host "[*] Number of events in the last $time_period hours is:" $all_events.Count
+
+# List unique values
+Write-Host "IDs of events seen in last 24 hours"
+$event_IDs = $all_events | Sort-Object -Property ID -Unique 
+$event_IDs
+
+# Get count of each type of event
+Write-Host "Get Counts"
+$all_events | Group-Object -Property id | Select-Object -Property Name,Count | Format-table 
+
+$some_events = $all_events | Where-Object { $_.id -eq 1 } | Select-Object -First 3
+
+foreach ( $something in $some_events.Message  ) {
+    foreach ( $thingy in $some_events ) {
+        Write-Host "-------------------"
+        $thingy
+    }
+}
+
+
